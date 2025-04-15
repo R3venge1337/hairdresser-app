76cb75d0d56441516bd5c8f591190881f9ce5bc6
Adam Zimny <adamczyk97@op.pl>
2025-04-15 02:06:29 +0200

    Add propertySource which will be reading variables from secret.properties

--- backend/src/main/java/pl/lodz/p/backend/BackendApplication.java
+++ backend/src/main/java/pl/lodz/p/backend/BackendApplication.java
@@ -2,8 +2,10 @@ package pl.lodz.p.backend;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.PropertySource;
 
 @SpringBootApplication
+@PropertySource("file:${user.dir}/secret.properties")
 public class BackendApplication {
 
 	public static void main(String[] args) {
