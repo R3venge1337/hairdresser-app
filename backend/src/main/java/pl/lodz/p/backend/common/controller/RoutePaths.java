9ddf254853750c9bc92f9dcb2f2b0445506a0267
Adam Zimny <adamczyk97@op.pl>
2025-04-16 13:05:37 +0200

    Add class for mappings route paths

---
+++ backend/src/main/java/pl/lodz/p/backend/common/controller/RoutePaths.java
@@ -0,0 +1,9 @@
+package pl.lodz.p.backend.common.controller;
+
+public class RoutePaths {
+    public static final String LOGIN =  "/login";
+    public static final String REGISTRATION = "/register";
+
+    private RoutePaths() {
+    }
+}
