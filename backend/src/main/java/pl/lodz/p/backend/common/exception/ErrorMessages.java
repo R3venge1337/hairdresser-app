4ad971f53a0493c0d6d6ce32cab07e82745e4e17
Adam Zimny <adamczyk97@op.pl>
2025-04-16 13:03:10 +0200

    Add classes for exception like messages,controllerAdvice or dtos

---
+++ backend/src/main/java/pl/lodz/p/backend/common/exception/ErrorMessages.java
@@ -0,0 +1,12 @@
+package pl.lodz.p.backend.common.exception;
+
+public class ErrorMessages {
+    public static final String USER_NOT_FOUND = "exception.user.not_found";
+    public static final String USER_EXIST_BY_NAME = "exception.user.exist_by_name";
+    public static final String USER_EXIST_BY_EMAIL = "exception.user.exist_by_email";
+
+    private ErrorMessages(){
+
+    }
+
+}
