4af2c2e0dc797311390967efe47b99ec74408f98
Adam Zimny <adamczyk97@op.pl>
2025-04-19 16:28:50 +0200

    Add register component

---
+++ frontend/src/app/components/register/register.component.ts
@@ -0,0 +1,66 @@
+import { Component } from '@angular/core';
+import {
+  FormBuilder,
+  FormGroup,
+  ReactiveFormsModule,
+  Validators,
+} from '@angular/forms';
+import { MatCardModule } from '@angular/material/card';
+import {
+  MatError,
+  MatFormField,
+  MatFormFieldControl,
+  MatLabel,
+} from '@angular/material/form-field';
+import { RegisterForm } from '../../models/register-form';
+import { MatInputModule } from '@angular/material/input';
+import { CommonModule } from '@angular/common';
+import { AuthService } from '../../services/auth.service';
+import { Router } from '@angular/router';
+import { MatButtonModule } from '@angular/material/button';
+import { MatToolbarModule } from '@angular/material/toolbar';
+
+@Component({
+  selector: 'app-register',
+  imports: [
+    CommonModule,
+    MatInputModule,
+    MatCardModule,
+    MatFormField,
+    MatLabel,
+    MatError,
+    ReactiveFormsModule,
+    MatToolbarModule,
+    MatButtonModule,
+  ],
+  templateUrl: './register.component.html',
+  styleUrl: './register.component.css',
+})
+export class RegisterComponent {
+  registerForm: FormGroup;
+
+  constructor(
+    private fb: FormBuilder,
+    private authService: AuthService,
+    private router: Router
+  ) {
+    this.registerForm = this.fb.group({
+      firstname: ['', Validators.required],
+      surname: ['', Validators.required],
+      email: ['', [Validators.required, Validators.email]],
+      username: ['', Validators.required],
+      password: ['', Validators.required],
+      phoneNumber: ['', Validators.required],
+    });
+  }
+
+  async onSubmit() {
+    if (this.registerForm.valid) {
+      const formData: RegisterForm = this.registerForm.value;
+      await this.authService.register(formData);
+      this.router.navigate(['/login']);
+    } else {
+      console.log('Formularz jest nieprawidłowy');
+    }
+  }
+}
---
+++ frontend/src/app/models/register-form.ts
@@ -0,0 +1,8 @@
+export interface RegisterForm {
+  firstname: string;
+  surname: string;
+  email: string;
+  username: string;
+  password: string;
+  phoneNumber: string;
+}
