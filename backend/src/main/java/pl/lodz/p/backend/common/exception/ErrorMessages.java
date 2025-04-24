aa558874fd120907bed8e5e4b6f075f8c58825b3
Adam Zimny <adamczyk97@op.pl>
2025-04-24 13:47:24 +0200

    Add error messages for appointment and hairoffers

--- backend/src/main/java/pl/lodz/p/backend/common/exception/ErrorMessages.java
+++ backend/src/main/java/pl/lodz/p/backend/common/exception/ErrorMessages.java
@@ -4,6 +4,10 @@ public class ErrorMessages {
     public static final String USER_NOT_FOUND = "exception.user.not_found";
     public static final String USER_EXIST_BY_NAME = "exception.user.exist_by_name";
     public static final String USER_EXIST_BY_EMAIL = "exception.user.exist_by_email";
+    public static final String APPOINTMENT_NOT_FOUND = "exception.appointment.not_found";
+    public static final String APPOINTMENT_EXIST = "exception.appointment.exist";
+    public static final String HAIR_OFFER_NOT_FOUND = "exception.hair_offer.not_found";
+    public static final String HAIR_OFFER_EXIST_BY_NAME = "exception.hair_offer.exist_by_name";
 
     private ErrorMessages(){
 
