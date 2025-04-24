87622be906314aa577acfae890ae01b4b1b92954
Adam Zimny <adamczyk97@op.pl>
2025-04-24 13:49:00 +0200

    Add route paths for appointments and users

--- backend/src/main/java/pl/lodz/p/backend/common/controller/RoutePaths.java
+++ backend/src/main/java/pl/lodz/p/backend/common/controller/RoutePaths.java
@@ -3,6 +3,9 @@ package pl.lodz.p.backend.common.controller;
 public class RoutePaths {
     public static final String LOGIN =  "/login";
     public static final String REGISTRATION = "/register";
+    public static final String HAIR_OFFERS = "/hair-offers";
+    public static final String APPOINTMENTS = "/appointments";
+    public static final String USERS = "/users";
 
     private RoutePaths() {
     }
