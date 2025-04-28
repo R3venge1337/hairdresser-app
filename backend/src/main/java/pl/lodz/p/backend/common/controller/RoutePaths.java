6c8e84d2f84eb91a9418157705435c49a248adfa
Adam Zimny <adamczyk97@op.pl>
2025-04-28 13:20:24 +0200

    Fix route path for hairoffers

--- backend/src/main/java/pl/lodz/p/backend/common/controller/RoutePaths.java
+++ backend/src/main/java/pl/lodz/p/backend/common/controller/RoutePaths.java
@@ -3,7 +3,7 @@ package pl.lodz.p.backend.common.controller;
 public class RoutePaths {
     public static final String LOGIN =  "/login";
     public static final String REGISTRATION = "/register";
-    public static final String HAIR_OFFERS = "/hair-offers";
+    public static final String HAIR_OFFERS = "/hairoffers";
     public static final String APPOINTMENTS = "/appointments";
     public static final String USERS = "/users";
 
