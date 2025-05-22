4130e7696511c64ee178b137202a76a6bf53a7f1
Adam Zimny <adamczyk97@op.pl>
2025-05-22 20:12:26 +0200

    Add route path for appointments in specific day

--- backend/src/main/java/pl/lodz/p/backend/common/controller/RoutePaths.java
+++ backend/src/main/java/pl/lodz/p/backend/common/controller/RoutePaths.java
@@ -6,6 +6,7 @@ public class RoutePaths {
     public static final String HAIR_OFFERS = "/hairoffers";
     public static final String APPOINTMENTS = "/appointments";
     public static final String USERS = "/users";
+    public static final String APPOINTMENTS_DATE = "/appointments-singleday";
 
     private RoutePaths() {
     }
